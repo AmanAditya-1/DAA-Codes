2c4e2133b0e4cb1487e542b43c32317dcd07b272
Aman Aditya <amanaditya599@gmail.com>
2022-10-15 17:14:04 +0530

    pushing quicksort updated code with pass arrays

--- Quick_Sort.cpp
+++ Quick_Sort.cpp
@@ -1,6 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int x=1;
+
+void printarray(int arr[],int n)
+{
+    x++;
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<"\n"<<endl;
+}
 int partition(int arr[], int p, int q)
 {
     int pi = arr[p]; // initiliazing and defining the pivot.
@@ -27,14 +38,17 @@ int partition(int arr[], int p, int q)
     return j;
 }
 
-void quicksort(int arr[], int p, int q)
+void quicksort(int arr[], int p, int q,int n)
 {
     if (p < q) // check if the array has more than one element.
     {
-        int loc = partition(arr, p, q); // we will get the new index of the pivot after the pivot function is executed.
+        int loc = partition(arr, p, q); // we will get the new index of the pivot after the partition function is executed.
         // next two quicksort functions are applied for two subarrays formed on the left and right of the pivot.
-        quicksort(arr, p, loc - 1);
-        quicksort(arr, loc + 1, q);
+        cout<<"pivot= "<<arr[loc]<<endl;
+        cout<<"Array after pass "<<x<<" is :"<<endl;
+        printarray(arr,n);
+        quicksort(arr, p, loc - 1,n);
+        quicksort(arr, loc + 1, q,n);
     }
 }
 
@@ -47,7 +61,8 @@ int main()
     {
         cin >> arr[i];
     }
-    quicksort(arr, 0, n - 1);
+    quicksort(arr, 0, n - 1,n);
+    cout<<"Final sorted array is : "<<endl;
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";

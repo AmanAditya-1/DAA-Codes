47f071c6a13f12e441adf6696e460ca205e4db10
Aman Aditya <amanaditya599@gmail.com>
2022-10-15 17:10:48 +0530

    Added counter variable in minmax and shown array after each pass in quicksort

--- min_max.cpp
+++ min_max.cpp
@@ -1,9 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int cnt=0; //Defining a global variable for counting the number of comparisions
+
 void minmax(int arr[], int l, int h, int &min, int &max)
 {
-    if (l == h)
+    if (l == h) //if array has only one element.
     {
         if (min > arr[l])
             min = arr[l];
@@ -12,7 +14,7 @@ void minmax(int arr[], int l, int h, int &min, int &max)
 
         return;
     }
-    else if (h - l == 1)
+    else if (h - l == 1) // if array has two elements.
     {
         if (arr[l] < arr[h])
         {
@@ -28,11 +30,17 @@ void minmax(int arr[], int l, int h, int &min, int &max)
             if (arr[l] > max)
                 max = arr[l];
         }
+        cnt++; //iif there are 2 elements in subarray then there will be only one comparision
         return;
     }
-    else
+    else //if array has more than two elements.
     {
+        cnt+=2;// if more than two elements in array than there will be 2 comparisions.
         int mid = (l + h) / 2;
+        if(mid%2==0) //the division of subarray will be two parts each having even number of elements.
+        {
+            mid++;
+        }
         minmax(arr, l, mid, min, max);
         minmax(arr, mid + 1, h, min, max);
     }
@@ -50,6 +58,6 @@ int main()
     minmax(arr, 0, n - 1, min, max);
     cout << "min= " << min << endl;
     cout << "max= " << max << endl;
-    cout<<"number of comparisions= "<<floor(((3*n)/2)-2)<<endl;
+    cout<<"number of comparisions= "<<cnt<<endl;
     return 0;
 }
